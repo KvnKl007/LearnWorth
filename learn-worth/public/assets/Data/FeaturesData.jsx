035f607f779a846a08f58c7c2947ead902c32499
KennyKvn001 <k.mugisha@alustudent.com>
2024-11-14 12:50:37 +0200

    Move assets to public folder and update paths

--- learn-worth/public/assets/Data/FeaturesData.jsx
+++ learn-worth/public/assets/Data/FeaturesData.jsx
@@ -2,17 +2,17 @@ export const features = [
     {
         title: 'Free Courses',
         description: 'Access a variety of courses designed to improve financial literacy.',
-        icon: '/public/assets/icons/books.png',
+        icon: '/assets/icons/books.png',
     },
     {
         title: 'Interactive Learning',
         description: 'Engage with interactive modules and real-life financial scenarios.',
-        icon: '/public/assets/icons/interactive.png',
+        icon: '/assets/icons/interactive.png',
     },
     {
         title: 'Test yourself',
         description: 'Quizes and flashcards that will help your learning expirience better.',
-        icon: '/public/assets/icons/test.png',
+        icon: '/assets/icons/test.png',
     },
     // Add more features as needed
 ];
